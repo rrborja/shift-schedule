e40b7dda5098287cc2008cccd7c2449eebe243f7
Ritchie Borja <code@brute.io>
2018-06-05 15:19:44 -0400

    New test cases for testing the Storage Handlers

---
+++ storage_test.go
@@ -0,0 +1,22 @@
+package schedule
+
+import (
+	"os"
+	"testing"
+)
+
+func TestSavedShift(t *testing.T) {
+	testRecord := DayRecord{6, 6, 2006}
+	testRecord.ClockIn(Employee{"Tester", 666}, 0, 24)
+	testRecord.ClockIn(Employee{"Finalizer", 777}, 24, 48)
+
+	if _, err := os.Stat(testRecord.String()); os.IsNotExist(err) {
+		t.Error("Test record was not checked in, thus not saved")
+	}
+
+	defer os.RemoveAll(testRecord.String())
+
+	if Construct(testRecord) == nil {
+		t.Error("Test record was not checked in, thus not saved")
+	}
+}
